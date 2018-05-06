e4d205e4c0e2937b7b5655ec7a87e09e56aecef6
Santa <s.yushmanov777@gmail.com>
2018-05-06 16:14:50 +0700

    Generators

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
-import { create } from "domain";
-import { DH_CHECK_P_NOT_SAFE_PRIME } from "constants";
-
-/*let s = Symbol("field");
-
-let obj = {
-  age: 20,
-  [s]: "WFM"
-};
-
-console.log(Object.getOwnPropertySymbols(obj));
-
-let num = 1;
-let str = "2";
-let arr = [1, 2];
-let obj = {name: "WFM"};
-
-console.log("Number: ", typeof num[Symbol.iterator]);
-console.log("String: ", typeof str[Symbol.iterator]);
-console.log("Array: ", typeof arr[Symbol.iterator]);
-console.log("Object: ", typeof obj[Symbol.iterator]);
-
-function creatItterator(arr) {
-  let count = 0;
-  return {
-    next() {
-      return count < arr.length
-       ? {value: arr[count++], done: false} : {value: undefined, done: true};
-    }
-  };
+/*function* gen() {
+  yield 11;
+  yield 22;
+  yield 33;
 }
-let iter = creatItterator([1,2,3,4]);
+
+let iter = gen();
+
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+
+function* g1() {
+  yield 1;
+  yield* g2();
+  yield 4;
+}
+function* g2() {
+  yield 2;
+  yield 3;
+}
+let iter = g1();
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
+
+function* g() {
+  yield* [1,2,3];
+}
+let iter = g();
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+console.log(iter.next());
+
+function* getRange(start = 0, end = 100, step = 10) {
+  while (start < end) {
+    yield start;
+    start += step;
+  }
+}
+
+for (let n of getRange(10, 50, 2)) {
+  console.log(n);
+}
 */
 let fib = {
-  [Symbol.iterator]() {
-    let pre = 0, cur = 1;
-    return {
-      next() {
-        [pre, cur] = [cur, pre + cur];
-        return {
-          value: cur, done: false
-        };
-      }
+  *[Symbol.iterator]() {
+    let cur = 1, pre = 0;
+    for(;;) {
+      [cur, pre] = [cur + pre, cur];
+      yield cur;
     };
   }
 };
 
-for( let n of fib) {
-  if(n > 1500) break;
-  console.log(n);
+for (let i of fib) {
+  if(i > 3000) break;
+  console.log(i);
 }
