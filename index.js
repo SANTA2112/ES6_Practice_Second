cb1b783d82c93cec3a4d55f7fad90ffce778f06c
Santa <s.yushmanov777@gmail.com>
2018-05-06 17:11:30 +0700

    Promise

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-/*let obj1 = {a: 1};
-let obj2 = {b: 2, c: 3};
+// http://date.jsontest.com/
 
-let obj3 = Object.assign({}, obj1, obj2);
-console.log(obj1, obj2, obj3);
+/*function oldDelay(ms, func) {
+  setTimeout(() => func(), ms);
+}
 
-let findItem = [1,2,3,4].find(x => x > 3);
+oldDelay(3000, () => console.log("Old Delay Passed!"));*/
 
-console.log(findItem);
+function delay(ms = 1000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(), ms);
+  });
+}
 
-
-let str = "Hello";
-console.log("Repeat: ", str.repeat(3));
-console.log("startsWith: ", str.startsWith("el", 1));
-console.log("Includes: ", str.includes("el", 1));
-*/
+delay(4000)
+  .then(() => {
+    console.log("new delay passed!");
+  })
+  .catch(() => {
+    console.info("error");
+  });
