e733f8b9edc75f27e53c3e5bc369f6ba6b3e3a92
Santa <s.yushmanov777@gmail.com>
2018-05-06 14:42:20 +0700

    Import & Export

---
+++ extra.js
@@ -0,0 +1,17 @@
+/*const name = "WFM";
+const age = 20;
+
+export {
+  age,
+  name
+};
+
+export default function func() {
+  console.log("I am func");
+}
+*/
+export default class Car {
+  constructor() {
+    console.log("Car constructor!");
+  }
+}
